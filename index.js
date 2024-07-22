4ccecf38062b4cf48479f9427a4e01e93dc39205
brightini <brightiniabasi@gmail.com>
2024-07-22 13:45:02 +0100

    Added index.js file

---
+++ index.js
@@ -0,0 +1,61 @@
+import express from "express";
+const app = express();
+import cors from "cors";
+
+const generateID = () => {
+  const maxID =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+
+  return String(maxID + 1);
+};
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateID(),
+  };
+
+  notes = notes.concat(note);
+  response.json(notes);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
